7bf79089bc66bf2da04f5f0af027f97764622e14
khadjafatima409 <khadijafatima530@gmail.com>
2023-11-24 20:35:01 -0800

    created store

--- src/Store/courseStore.js
+++ src/Store/courseStore.js
@@ -1,1 +1,30 @@
 import create from "zustand";
+import { devtools, presist } from "zustand/middleware";
+
+const courseStore = (set) => ({
+  courses: [],
+  addCourse: (course) => {
+    set((state) => ({
+      courses: [course, ...state.courses],
+    }));
+  },
+  removeCourse: (courseId) => {
+    set((state) => ({
+      courses: state.courses.filter((c) => c.id !== courseId),
+    }));
+  },
+  toggleCourseStatus: (courseId) => {
+    set((state) => ({
+      courses: state.courses.map((course) =>
+        // eslint-disable-next-line no-undef
+        course.id === completed
+          ? { ...course, completed: !course.completed }
+          : course
+      ),
+    }));
+  },
+});
+const useCourseStore = create(
+  devtools(presist(courseStore, { name: "courses" }))
+);
+export default useCourseStore;
